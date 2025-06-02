16c2fef9e98624017ec80ac56f8dfae5074fb9fd
GGOOFPRO <kitsana.sittibut@gmail.com>
2025-06-02 16:49:10 +0700

    V0.3 Dah Saving Has COOM!!

--- let_it_load/lib/controller/loading_controller.dart
+++ let_it_load/lib/controller/loading_controller.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class LoadingController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -19,71 +20,91 @@ class LoadingController extends GetxController
       ),
     )..repeat();
   }
+
+  void updateAnimationSpeed() {
+    anicontroller.duration = Duration(
+      milliseconds: controller.pointPerSecondAnimation.value,
+    );
+    anicontroller
+      ..reset()
+      ..repeat();
+  }
 }
 
 class LoadingPoint extends GetxController {
-  var point = 0.0.obs;
+  final box = GetStorage();
 
-  /////////////////////////////////////////////////
+  var point = 0.0.obs;
   var pointMultiplyer = 1.0.obs;
   var pointMultiplyerPrice = 20.0.obs;
-  var pointMultiplyerBuyable = false.obs;
   var pointMultiplyerAmount = 1.obs;
 
-  /////////////////////////////////////////////////
-  var speedUpBuyable = false.obs;
-  var speedUpPrice = 50.00.obs;
+  var speedUpPrice = 50.0.obs;
   var pointPerSecond = 1000.obs;
   var pointPerSecondAnimation = 2000.obs;
-  var speedUpAmount = 0.obs;
 
   @override
   void onInit() {
     super.onInit();
+    loadProgress();
     pointUp();
+  }
+
+  void loadProgress() {
+    point.value = box.read('point') ?? 0.0;
+    pointMultiplyer.value = box.read('multiplier') ?? 1.0;
+    pointMultiplyerAmount.value = pointMultiplyer.value.toInt();
+    pointMultiplyerPrice.value = box.read('multiplierPrice') ?? 20.0;
+    speedUpPrice.value = box.read('speedUpPrice') ?? 50.0;
+    pointPerSecond.value = box.read('pointPerSecond') ?? 1000;
+    pointPerSecondAnimation.value = pointPerSecond.value * 2;
+  }
+
+  void saveProgress() {
+    box.write('point', point.value);
+    box.write('multiplier', pointMultiplyer.value);
+    box.write('multiplierPrice', pointMultiplyerPrice.value);
+    box.write('speedUpPrice', speedUpPrice.value);
+    box.write('pointPerSecond', pointPerSecond.value);
   }
 
   Future<void> pointUp() async {
     while (true) {
       await Future.delayed(Duration(milliseconds: pointPerSecond.value));
-      point.value += 1.0 * pointMultiplyer.value;
+      point.value += pointMultiplyer.value;
+      saveProgress(); // Auto-save
     }
   }
 
   void buyPointMultiplyer() {
     if (point.value >= pointMultiplyerPrice.value) {
-      pointMultiplyerBuyable.value = true;
       point.value -= pointMultiplyerPrice.value;
       pointMultiplyerAmount.value += 1;
-
-      pointMultiplyer.value += 1.0;
+      pointMultiplyer.value += 1;
 
       double scale = 1.5 - (0.05 * (pointMultiplyer.value - 1));
       scale = scale.clamp(1.1, 1.5);
       double spike = Random().nextDouble() * 0.5 + 1.0;
-      pointMultiplyerPrice.value *= spike;
+      pointMultiplyerPrice.value *= scale * spike;
+      pointMultiplyerPrice.value = double.parse(pointMultiplyerPrice.value.toStringAsFixed(2));
 
-      pointMultiplyerPrice.value *= scale;
-      pointMultiplyerPrice.value = double.parse(
-        pointMultiplyerPrice.value.toStringAsFixed(2),
-      );
-    } else {
-      pointMultiplyerBuyable.value = false;
+      saveProgress();
     }
   }
 
   void buySpeedUP() {
     if (point.value >= speedUpPrice.value) {
-      speedUpBuyable.value = true;
       point.value -= speedUpPrice.value;
 
-      pointPerSecond.value -= 2;
-      pointPerSecondAnimation.value -= 4;
+      pointPerSecond.value = max(100, pointPerSecond.value - 2);
+      pointPerSecondAnimation.value = max(200, pointPerSecondAnimation.value - 4);
 
       double spike = Random().nextDouble() * 0.5 + 1.0;
       speedUpPrice.value *= spike;
-
       speedUpPrice.value = double.parse(speedUpPrice.value.toStringAsFixed(2));
+
+      Get.find<LoadingController>().updateAnimationSpeed();
+      saveProgress();
     }
   }
 }
--- let_it_load/lib/main.dart
+++ let_it_load/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:let_it_load/view/homepage.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
   runApp(const MyApp());
 }
 
